0e0f876e5e85d4f69d9bb8fd8b6432463c54a03c
Kevin-Veenboer <135600479+Kevin-Veenboer@users.noreply.github.com>
2023-11-02 12:00:40 +0100

    Added testing to the ELM script

--- ElectronicLoadMeasurement.py
+++ ElectronicLoadMeasurement.py
@@ -7,15 +7,17 @@ class ElectronicLoadMeasurement:
 
     # Method to add measurement to instance
     def add_Measurement(self, R, U):
+        assert R != 0, "Loads of zero are not allowed"
+
         # append load and volatage to storage lists directly
         self.Loads.append(R)
         self.Voltages.append(U)
 
         # Calculate the current with voltage/load and store teh result
-        self.currents.append(U / R)
+        self.Currents.append(U / R)
 
         # calculate the power with (U**2/R) this is equal to (I**2 * R), store the result
-        self.powers.append(U**2 / R)
+        self.Powers.append(U**2 / R)
 
     # Methods to retrieve the measurements from an instance
     def get_loads(self):
@@ -36,3 +38,24 @@ class ElectronicLoadMeasurement:
         self.Voltages = list()
         self.Currents = list()
         self.Powers = list()
+
+
+## Testing the class
+
+# Creating and adding a few measurement
+ELM = ElectronicLoadMeasurement()
+for volt in range(2, 102, 2):
+    ELM.add_Measurement(200, volt)
+
+# Testing the data retrieval
+print("\nLists with data:")
+print(
+    f"Loads: {ELM.get_loads()}\n\nVoltages: {ELM.get_voltages()}\n\nCurrents: {ELM.get_currents()}\n\nPowers: {ELM.get_powers()}"
+)
+
+# Testing clear function by making sure data retrieval gives empty lists after using the method
+ELM.clear()
+print("\nLists are empty after .clear() method was used")
+print(
+    f"Loads: {ELM.get_loads()}\n\nVoltages: {ELM.get_voltages()}\n\nCurrents: {ELM.get_currents()}\n\nPowers: {ELM.get_powers()}"
+)
